75692811afd18dd8e239f500ac9a272a3a4aef7e
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2020-11-10 15:30:05 -0300

    console => api express++, GET courses, file refactoring

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
 
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`)
+const { readFileSync } = require('fs')
+const { join } = require('path')
 
-const resolvers = {
-    hello: () => {
-        return "Hola mundo!"
-    }
-}
- 
-graphql(schema, '{ hello }', resolvers).then(result => console.log(result))
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+console.log()
+
+const schema = buildSchema(readFileSync(
+  join(__dirname, 'lib', 'schema.graphql')
+).toString('utf8'))
+
+app.use('/api', graphqlHTTP({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
+
+// = hola mundo consola
+// graphql(schema, '{ hello }', resolvers).then(result => console.log(result))
